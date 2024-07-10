180ac87eb8d83d1bb32111808f3707cf061ddca9
DiegoCoura <diegogomes.10.9@gmail.com>
2024-07-10 20:17:55 -0300

    fix: try catch on mongoose connection to DB

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-require('dotenv').config()
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+require("dotenv").config();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const catalogRouter = require("./routes/catalog"); 
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
-const compression = require('compression')
+const compression = require("compression");
 const helmet = require("helmet");
 
 const app = express();
 
 // Set up mongoose connection
-const mongoose = require('mongoose')
-mongoose.set("strictQuery", false)
-const mongoDB = process.env.dbURI
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.dbURI;
 
 main().catch((err) => console.log(err));
-async function main (){
-  await mongoose.connect(mongoDB)
+async function main() {
+  try {
+    await mongoose.connect(mongoDB);
+  } catch (err) {
+    console.log("error: " + err);
+  }
 }
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
       "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
     },
-  }),
+  })
 );
 
 // Set up rate limiter: maximum of twenty requests per minute
@@ -46,33 +49,32 @@ const limiter = RateLimit({
 // Apply rate limiter to all requests
 app.use(limiter);
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(compression()); // Compress all routes
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(compression()) // Compress all routes
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
